cdc1002dea1ebb08b655db53f6629b633a45a297
김동욱 <kdw115@ubcare.co.kr>
2019-09-03 10:57:27 +0900

    Add cover images

--- lib/main.dart
+++ lib/main.dart
@@ -55,45 +55,175 @@ class _MyHomePageState extends State<MyHomePage> {
       '😽',
       '😻',
       '😺',
-      "✌","😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"
-
+      "✌",
+      "😂",
+      "😝",
+      "😁",
+      "😱",
+      "👉",
+      "🙌",
+      "🍻",
+      "🔥",
+      "🌈",
+      "☀",
+      "🎈",
+      "🌹",
+      "💄",
+      "🎀",
+      "⚽",
+      "🎾",
+      "🏁",
+      "😡",
+      "👿",
+      "🐻",
+      "🐶",
+      "🐬",
+      "🐟",
+      "🍀",
+      "👀",
+      "🚗",
+      "🍎",
+      "💝",
+      "💙",
+      "👌",
+      "❤",
+      "😍",
+      "😉",
+      "😓",
+      "😳",
+      "💪",
+      "💩",
+      "🍸",
+      "🔑",
+      "💖",
+      "🌟",
+      "🎉",
+      "🌺",
+      "🎶",
+      "👠",
+      "🏈",
+      "⚾",
+      "🏆",
+      "👽",
+      "💀",
+      "🐵",
+      "🐮",
+      "🐩",
+      "🐎",
+      "💣",
+      "👃",
+      "👂",
+      "🍓",
+      "💘",
+      "💜",
+      "👊",
+      "💋",
+      "😘",
+      "😜",
+      "😵",
+      "🙏",
+      "👋",
+      "🚽",
+      "💃",
+      "💎",
+      "🚀",
+      "🌙",
+      "🎁",
+      "⛄",
+      "🌊",
+      "⛵",
+      "🏀",
+      "🎱",
+      "💰",
+      "👶",
+      "👸",
+      "🐰",
+      "🐷",
+      "🐍",
+      "🐫",
+      "🔫",
+      "👄",
+      "🚲",
+      "🍉",
+      "💛",
+      "💚"
     ];
-    final texts = ['막 바뀜', '테스트 문구', '테스트', '정훈찡', '당신은 사랑받기 위해 출근한 사람', '쉬면서 해요', '기분좋아져라 얍'];
-
+    final texts = [
+      '막 바뀜',
+      '테스트 문구',
+      '테스트',
+      '정훈찡',
+      '당신은 사랑받기 위해 출근한 사람',
+      '쉬면서 해요',
+      '기분좋아져라 얍'
+    ];
 
     final emotionIndex = Random().nextInt(emotions.length);
     final textIndex = Random().nextInt(texts.length);
+
     return Scaffold(
-      
+      backgroundColor: Color(0xFF222222),
       body: GestureDetector(
-        onTap: (){
-          setState(() {
-            
-          });
+        onTap: () {
+          setState(() {});
         },
-        child: Container(
-          color: Colors.grey[200],
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                emotions[emotionIndex],
-                style: TextStyle(fontSize: 128, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                texts[textIndex],
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 64,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(
-                    Random().nextInt(225),
-                    Random().nextInt(225), 
-                    Random().nextInt(225), 1.0),
+        child: SafeArea(
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  child: Image.asset(
+                    'images/cover1.png',
+                    width: 600,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          emotions[emotionIndex],
+                          style: TextStyle(
+                              fontSize: 128, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          texts[textIndex],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: texts[textIndex].length >= 8 ? 38 : 54,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(
+                                Random().nextInt(225),
+                                Random().nextInt(225),
+                                Random().nextInt(225),
+                                1.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  child: Image.asset(
+                    'images/cover2.png',
+                    width: 600,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
